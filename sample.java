e99045ea631707986ff5d07f95f4b47a79094f5a
sanjaythrinad <sanjaythrinad@gmail.com>
2024-05-18 04:23:58 +0000

    Update sample.java

--- sample.java
+++ sample.java
@@ -2,6 +2,7 @@ class HelloWorld
   {
     public static void main(String []args)
     {
-      System.out.println("Java sample code that runs from Jenkins");
+      System.out.println("Java sample code that runs from Jenkins...");
+      System.out.println("Poll SCM testing...!!!");
     }
   };
